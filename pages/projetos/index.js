f770cac15a5357ee93caf3127c510a5b9d187e83
Renan Brayner <renanbrayner@gmail.com>
2020-11-14 17:43:11 -0300

    Refactor: refactored projects

--- pages/projetos/index.js
+++ pages/projetos/index.js
@@ -1,67 +1,53 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import {
+	useState
+} from 'react'
 import Container from './styles'
 
 import projectsData from '../../utils/projectData'
 
 function Projetos() {
-  const [project, setProject] = useState(projectsData.cats)
+	const [project, setProject] = useState(projectsData[0])
 
-  const handleClick = (newProject) => {
-    setProject(newProject)
-  }
+	const handleClick = (newProject) => {
+		setProject(newProject)
+	}
 
-  return (
-    <Container>
-      <Head>
-        <title>Renan Brayner | Meus projetos</title>
-        <meta name="description" content="Feito com NodeJS, ReactJS, React Native e mongodb" />
-      </Head>
-      <div className='grid'>
-        <img 
-          className='project' 
-          src={projectsData.cats.img} 
-          onClick={() => handleClick(projectsData.cats)}
-        />
-        <img
-          className='project'
-          src={projectsData.todo.img}
-          onClick={() => handleClick(projectsData.todo)}
-        />
-        <img
-          className='project'
-          src={projectsData.beTheHero.img}
-          onClick={() => handleClick(projectsData.beTheHero)}
-        />
-        <img 
-          className='project'
-          src={projectsData.borderRadius.img}
-          onClick={() => handleClick(projectsData.borderRadius)}
-        />
-        <img 
-          className='project'
-          src={projectsData.sorteador.img}
-          onClick={() => handleClick(projectsData.sorteador)}
-        />
-        <img 
-          className='project'
-          src={projectsData.jogoDaVelha.img}
-          onClick={() => handleClick(projectsData.jogoDaVelha)}
-        />
-      </div>
-      <div className='selected'>
-        <img className='big' src={project.img} />
-        <div className='text-container'>
-          <h1 className='title'>{project.title}</h1> 
-          <p className='text'>{project.description}</p>
-          <div className='buttons'>
-            <a className='repo' href={project.repository} target='_blank' rel='noreferrer'>repositorio</a> 
-            { project.site ? <a href={project.site} target='_blank' rel='noreferrer'>site</a> : null}
-          </div>
-        </div>
-      </div>
-    </Container>
-  )
+	return (
+		<Container>
+			<Head>
+				<title>Renan Brayner | Meus projetos</title>
+				<meta name="description" content="Feito com NodeJS, ReactJS, React Native e mongodb" />
+			</Head>
+				<div className='grid'>
+					{
+						projectsData.map((_, i) => {
+							return (
+								<img
+									className='project'
+									src={projectsData[i].img}
+									key={i}
+									onClick={() => handleClick(projectsData[i])}
+								/>
+							)
+						})
+					}
+				</div>
+				<div className='selected'>
+					<img className='big' src={project.img} />
+					<div className='text-container'>
+						<h1 className='title'>{project.title}</h1>
+						<p className='text'>{project.description}</p>
+						<div className='buttons'>
+							<a className='repo' href={project.repository} target='_blank' rel='noreferrer'>
+								repositorio
+							</a>
+							{ project.site ? <a href={project.site} target='_blank' rel='noreferrer'>site</a> : null}
+						</div>
+					</div>
+				</div>
+		</Container>
+	)
 }
 
 export default Projetos
