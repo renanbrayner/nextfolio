6059d55fff56de826e5c8d0bc850803eadb54355
Renan Brayner <renanbrayner@gmail.com>
2024-01-29 12:55:36 -0300

    Alterar botão de menu

--- src/app/[lang]/components/Header/MenuBtn.tsx
+++ src/app/[lang]/components/Header/MenuBtn.tsx
@@ -6,11 +6,44 @@ import { FiMenu } from "react-icons/fi";
 export default function MenuBtn() {
   const {
     actions: { toggleMenu },
+    state: { isMenuOpen },
   } = useLayoutStore();
 
   return (
-    <button onClick={() => toggleMenu()}>
-      <FiMenu className="text-xl" />
+    <button onClick={() => toggleMenu()} className="group relative">
+      <div
+        data-open={isMenuOpen}
+        className="relative flex h-10 w-10 transform items-center justify-center overflow-hidden rounded-full shadow-md ring-0 ring-slate-500 ring-opacity-30 transition-all duration-200 hover:ring-4 data-[open=true]:ring-2 data-[open=true]:hover:ring-4"
+      >
+        <div className="flex h-[20px] w-[20px] origin-center transform flex-col justify-around overflow-hidden transition-all duration-300">
+          <div
+            data-open={isMenuOpen}
+            className="h-[2px] origin-left transform bg-white transition-all duration-300 data-[open=true]:translate-x-10"
+          ></div>
+          <div
+            data-open={isMenuOpen}
+            className="h-[2px] transform rounded bg-white transition-all delay-75 duration-300 data-[open=true]:translate-x-10"
+          ></div>
+          <div
+            data-open={isMenuOpen}
+            className="h-[2px] origin-left transform bg-white transition-all delay-150 duration-300 data-[open=true]:translate-x-10"
+          ></div>
+
+          <div
+            data-open={isMenuOpen}
+            className="absolute top-2.5 flex w-0 -translate-x-10 transform items-center justify-between transition-all duration-500 data-[open=true]:w-12 data-[open=true]:translate-x-0"
+          >
+            <div
+              data-open={isMenuOpen}
+              className="absolute h-[2px] w-5 rotate-0 transform bg-white transition-all delay-300 duration-500 data-[open=true]:rotate-45"
+            ></div>
+            <div
+              data-open={isMenuOpen}
+              className="absolute h-[2px] w-5 -rotate-0 transform bg-white transition-all delay-300 duration-500 data-[open=true]:-rotate-45"
+            ></div>
+          </div>
+        </div>
+      </div>
     </button>
   );
 }
