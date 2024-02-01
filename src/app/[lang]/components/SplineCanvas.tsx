9b7f18e0b1669d20c88e00f948d1952bafc84c2b
Renan Brayner <renanbrayner@gmail.com>
2024-01-31 23:09:35 -0300

    mudar método de carregamento de 3d

--- src/app/[lang]/components/SplineCanvas.tsx
+++ src/app/[lang]/components/SplineCanvas.tsx
@@ -1,19 +1,10 @@
 "use client";
-
-import Script from "next/script";
+import Spline from "@splinetool/react-spline";
 
 export default function SplineCanvas({ className }: { className: string }) {
   return (
-    <>
-      <Script
-        type="module"
-        src="https://unpkg.com/@splinetool/viewer@1.0.38/build/spline-viewer.js"
-      />
-      <spline-viewer
-        class={className}
-        id="spline-viewer"
-        url="https://prod.spline.design/zSwC5uXGJe1OG2AN/scene.splinecode"
-      ></spline-viewer>
-    </>
+    <div className={className}>
+      <Spline scene="https://prod.spline.design/zSwC5uXGJe1OG2AN/scene.splinecode" />
+    </div>
   );
 }
