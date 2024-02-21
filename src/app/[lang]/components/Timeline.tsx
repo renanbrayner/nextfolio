801662a7d4f9b3419663ab0f149bb820b41b60c2
Renan Brayner <renanbrayner@gmail.com>
2024-02-20 22:58:20 -0300

    mudar estilização da timeline

--- src/app/[lang]/components/Timeline.tsx
+++ src/app/[lang]/components/Timeline.tsx
@@ -21,8 +21,8 @@ export default function Timeline({ items }: { items: TimelineItem[] }) {
   }) => {
     if (side === "left") {
       return (
-        <div className="flex flex-row-reverse md:contents">
-          <div className="col-start-1 col-end-5 my-4 ml-auto flex flex-col rounded-xl bg-slate-500 p-4 shadow-md">
+        <div className="group flex flex-row-reverse md:contents">
+          <div className="col-start-1 col-end-5 my-4 ml-auto flex flex-col rounded-xl border border-slate-700/50 bg-slate-900/50 p-4 shadow-md transition-all group-hover:scale-105 group-hover:shadow-xl">
             <h3 className="mb-1 text-lg font-semibold">{title}</h3>
             <p className="text-justify leading-tight">{children}</p>
             <span className="ml-auto flex items-center gap-1 pt-2">
@@ -33,7 +33,7 @@ export default function Timeline({ items }: { items: TimelineItem[] }) {
             <div className="flex h-full w-6 items-center justify-center">
               <div className="pointer-events-none h-full w-1 bg-slate-800"></div>
             </div>
-            <div className="absolute top-1/2 -mt-3 h-6 w-6 rounded-full bg-slate-500 shadow"></div>
+            <div className="absolute top-1/2 -mt-3 h-6 w-6 rounded-full bg-slate-900 shadow group-hover:scale-110 group-hover:shadow-xl"></div>
           </div>
         </div>
       );
@@ -41,14 +41,14 @@ export default function Timeline({ items }: { items: TimelineItem[] }) {
 
     if (side === "right") {
       return (
-        <div className="flex md:contents">
+        <div className="group flex md:contents">
           <div className="relative col-start-5 col-end-6 mr-10 md:mx-auto">
             <div className="flex h-full w-6 items-center justify-center">
               <div className="pointer-events-none h-full w-1 bg-slate-800"></div>
             </div>
-            <div className="absolute top-1/2 -mt-3 h-6 w-6 rounded-full bg-slate-500 shadow"></div>
+            <div className="absolute top-1/2 -mt-3 h-6 w-6 rounded-full bg-slate-900 shadow  transition-all group-hover:scale-110 group-hover:shadow-xl"></div>
           </div>
-          <div className="col-start-6 col-end-10 my-4 mr-auto flex flex-col rounded-xl bg-slate-500 p-4 shadow-md">
+          <div className="col-start-6 col-end-10 my-4 mr-auto flex flex-col rounded-xl border border-slate-700/50 bg-slate-900/50 p-4 shadow-md  transition-all group-hover:scale-105 group-hover:shadow-xl">
             <h3 className="mb-1 text-lg font-semibold">{title}</h3>
             <p className="text-justify leading-tight">{children}</p>
             <span className="ml-auto flex items-center gap-1 pt-2">
