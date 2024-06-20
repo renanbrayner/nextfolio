e063dd04a2339e9153f0296cfb1dd4776163d86b
Renan Brayner <renanbrayner@gmail.com>
2024-06-19 21:54:18 -0300

    mudar velocidade da animação

--- src/app/[lang]/components/Timeline.tsx
+++ src/app/[lang]/components/Timeline.tsx
@@ -22,7 +22,7 @@ export default function Timeline({ items }: { items: TimelineItem[] }) {
     if (side === "left") {
       return (
         <div className="group flex flex-row-reverse md:contents">
-          <div className="col-start-1 col-end-5 my-4 ml-auto flex flex-col rounded-xl border border-slate-700/50 bg-slate-900/50 p-4 shadow-md transition-all group-hover:scale-105 group-hover:shadow-xl">
+          <div className="col-start-1 col-end-5 my-4 ml-auto flex flex-col rounded-xl border border-slate-700/50 bg-slate-900/50 p-4 shadow-md transition-all duration-300 group-hover:scale-105 group-hover:shadow-xl">
             <h3 className="mb-1 text-lg font-semibold">{title}</h3>
             <p className="text-justify leading-tight">{children}</p>
             <span className="ml-auto flex items-center gap-1 pt-2">
@@ -48,7 +48,7 @@ export default function Timeline({ items }: { items: TimelineItem[] }) {
             </div>
             <div className="absolute top-1/2 -mt-3 h-6 w-6 rounded-full bg-slate-900 shadow  transition-all group-hover:scale-110 group-hover:shadow-xl"></div>
           </div>
-          <div className="col-start-6 col-end-10 my-4 mr-auto flex flex-col rounded-xl border border-slate-700/50 bg-slate-900/50 p-4 shadow-md  transition-all group-hover:scale-105 group-hover:shadow-xl">
+          <div className="col-start-6 col-end-10 my-4 mr-auto flex flex-col rounded-xl border border-slate-700/50 bg-slate-900/50 p-4 shadow-md transition-all  duration-300 group-hover:scale-105 group-hover:shadow-xl">
             <h3 className="mb-1 text-lg font-semibold">{title}</h3>
             <p className="text-justify leading-tight">{children}</p>
             <span className="ml-auto flex items-center gap-1 pt-2">
